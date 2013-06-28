09119fdcf0dc87e5908c8979db81b2914d5e46ce
Greg Lewis <gregwlewis@gmail.com>
2013-06-28 09:12:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scribe2::Application.config.secret_key_base = '551bba240cfdd083f276cdbca90bb3a4f5f4d8b12fdd65ed228faa3b4ac62bda754fb595d88525eb953cc6e4beb09f8b494f243f50e70a577f072d2dace8d6f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scribe2::Application.config.session_store :cookie_store, key: '_scribe2_session'
