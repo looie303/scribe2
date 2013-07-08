513b3a33aca92f4df314410dc7437dc3b5ef8b7e
Greg Lewis <gregwlewis@gmail.com>
2013-07-07 21:18:59 -0400

    8.2.1 completed with user model tests passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Sign the user in and redirect
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
