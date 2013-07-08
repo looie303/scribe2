4f7b3fed331aa2cbcf52050f8846397c925a8e8a
Greg Lewis <gregwlewis@gmail.com>
2013-07-07 21:47:23 -0400

    8.2.5 completed all tests passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to Scribe2"
+      sign_in @user
+      flash[:success] = "Welcome to Scribe2!"
       redirect_to @user
     else
       render 'new'
