a9b28f9747ffa083c6ea109f98ffcd6b85d846bb
Greg Lewis <looieloo@gmail.com>
2013-07-22 08:35:12 -0400

    9.2.1 complete all tests green

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -39,5 +40,11 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, 
                                     :password_confirmation)
+    end
+    
+    # Before filters
+    
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 end
