ef20a8d0b79580b108089030d69f6ae346b73d7f
Greg Lewis <looieloo@gmail.com>
2013-07-22 08:46:19 -0400

    9.2.2 completed all tests green

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,8 +16,12 @@ module SessionsHelper
   end
   
   def current_user
-    remember_token = cookies[:remember_token]
-    @current_user ||= User.find_by(remember_token: User.encrypt(remember_token))
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+  
+  def current_user?(user)
+    user == current_user
   end
   
   def sign_out
