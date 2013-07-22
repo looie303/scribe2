a82b5916175fd5431e37a210406f3699948fe95e
Greg Lewis <looieloo@gmail.com>
2013-07-22 11:28:37 -0400

    9.3.1 completed all tests green

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
   def show
@@ -32,6 +32,14 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.all
   end
   
   private
